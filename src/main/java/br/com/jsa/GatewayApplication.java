66f5f5b62508d720e78220d7e8d7a7394ea2d7e4
Jefferson Siqueira <jefferson08jsa@hotmail.com>
2022-06-25 09:27:33 -0300

    criando dockerfile e logbak

--- src/main/java/br/com/jsa/GatewayApplication.java
+++ src/main/java/br/com/jsa/GatewayApplication.java
@@ -2,6 +2,7 @@ package br.com.jsa;
 
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,6 +14,31 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 public class GatewayApplication {
 
+
+	@Value("${app.protocolo}")
+	private String protocolo;
+
+	@Value("${app.oauth.url}")
+	private String oauthUrl;
+	@Value("${app.oauth.port}")
+	private String oauthPort;
+	
+	@Value("${app.usuario.url}")
+	private String usuarioUrl;
+	@Value("${app.usuario.port}")
+	private String usuarioPort;
+	
+	@Value("${app.cadastroBasico.url}")
+	private String cadastroBasicoUrl;
+	@Value("${app.cadastroBasico.port}")
+	private String cadastroBasicoPort;
+	
+	@Value("${app.studio.url}")
+	private String studioUrl;
+	@Value("${app.studio.port}")
+	private String studioPort;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
@@ -23,23 +49,23 @@ public class GatewayApplication {
 				.route("autenticacao_route", r -> r
 						.path("/autenticacao/**")
 						.filters( f-> f.rewritePath("/autenticacao", ""))
-						.uri("http://localhost:8088"))
+						.uri(protocolo+"://"+oauthUrl+":"+oauthPort))
 				.route("usuario_route", r -> r
 						.path("/usuario/**")
 						.filters( f-> f.rewritePath("/usuario", ""))
-						.uri("http://localhost:8081"))
+						.uri(protocolo+"://"+usuarioUrl+":"+usuarioPort))
 				.route("cadastro_basico_route", r -> r
 					.path("/cadastro-basico/**")
 					.filters( f-> f.rewritePath("/cadastro-basico", "")
 							.setRequestHeader("Accept", MediaType.APPLICATION_JSON)
 							.setRequestHeader("Content-type", MediaType.APPLICATION_JSON))
-					.uri("http://localhost:8082"))
+					.uri(protocolo+"://"+cadastroBasicoUrl+":"+cadastroBasicoPort))
 				.route("studio_route", r -> r
 						.path("/studio/**")
 						.filters( f-> f.rewritePath("/studio", "")
 								.setRequestHeader("Accept", MediaType.APPLICATION_JSON)
 								.setRequestHeader("Content-type", MediaType.APPLICATION_JSON))
-						.uri("http://localhost:8083"))
+						.uri(protocolo+"://"+studioUrl+":"+studioPort))
 				.build();
 	}
 
